dada3998f391c3abe905960ed49b5deb8db4108f
Janina Kozachenko <janina.kozachenko@privatbank.ua>
2022-01-17 14:07:31 +0200

    init commit

--- src/com/pb/kozachenko/hw15/Client.java
+++ src/com/pb/kozachenko/hw15/Client.java
@@ -1,12 +1,48 @@
 package com.pb.kozachenko.hw15;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+public class Client  extends JFrame {
+
+
     public static void main(String[] args) throws Exception {
+        JFrame myWindow = new JFrame("JFrame");
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myWindow.setSize(400, 300);
+        myWindow.setVisible(true);
+
+        JTextField smallField = new JTextField(15);
+        smallField.setToolTipText("Короткое поле");
+        // Слушатель окончания ввода
+      smallField.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+                // Отображение введенного текста
+            String textFieldValue =smallField.getText();
+           }
+      });
+
+
+
+        myWindow.setLayout(new FlowLayout());
+
+        JButton jButtonSend = new JButton("Отправить на сервер");
+
+        jButtonSend.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                JOptionPane.showMessageDialog(null,"Привет","Нажато",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+        });
+        myWindow.add(smallField);
+        myWindow.add(jButtonSend);
+
         System.out.println("Клиент стартовал");
         String serverIp = "127.0.0.1";
         int port = 1234;
